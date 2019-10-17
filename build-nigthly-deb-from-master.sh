75cfe3adbbbe96054f9b1080acd98a194f3d7c90
Damien Ciabrini <damien.ciabrini@gmail.com>
2019-10-17 22:50:26 +0200

    Initial debian build branch

---
+++ build-nigthly-deb-from-master.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+set -ex
+
+export DEBFULLNAME="bot"
+export DEBEMAIL="bot@address.local"
+
+PROJECT=ngdevkit-gngeo
+UPSTREAM_VERSION=$(git grep AC_INIT origin/standalone:configure.ac | sed -ne 's/.*\[\(.*\)\].*/\1/p')
+read DATE SHORTHASH LONGHASH <<<$(git log -1 --date=format:"%Y%m%d%H%M" --pretty=format:"%cd %h %H" origin/standalone)
+DEB_VERSION=${UPSTREAM_VERSION}~${DATE}.${SHORTHASH}
+
+dch -v ${DEB_VERSION}-1 -U "Nightly build from tag ${LONGHASH}"
+git archive --format=tar --prefix=${PROJECT}-${DEB_VERSION}/ origin/standalone | gzip -c > ${PROJECT}_${DEB_VERSION}.orig.tar.gz
+tar xf ${PROJECT}_${DEB_VERSION}.orig.tar.gz
+cd ${PROJECT}-${DEB_VERSION}
+cp -a ../debian .
+yes | mk-build-deps --install --remove
+dpkg-buildpackage -rfakeroot -us -uc
