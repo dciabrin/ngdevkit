49c0b7268ccb42f1931f2124a766f82282517e23
Damien Ciabrini <damien.ciabrini@gmail.com>
2019-10-19 21:35:27 +0200

    Fix build control and rules

--- build-nigthly-deb-from-master.sh
+++ build-nigthly-deb-from-master.sh
@@ -5,7 +5,7 @@ set -ex
 export DEBFULLNAME="bot"
 export DEBEMAIL="bot@address.local"
 
-PROJECT=ngdevkit-gngeo
+PROJECT=ngdevkit
 UPSTREAM_VERSION=$(git grep AC_INIT origin/standalone:configure.ac | sed -ne 's/.*\[\(.*\)\].*/\1/p')
 read DATE SHORTHASH LONGHASH <<<$(git log -1 --date=format:"%Y%m%d%H%M" --pretty=format:"%cd %h %H" origin/standalone)
 DEB_VERSION=${UPSTREAM_VERSION}~${DATE}.${SHORTHASH}
