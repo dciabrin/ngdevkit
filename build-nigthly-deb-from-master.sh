e0ebc438f8615bba96dc77565f9920fa7266660f
Damien Ciabrini <damien.ciabrini@gmail.com>
2019-11-25 11:31:43 +0100

    Fix changelog generation

--- build-nigthly-deb-from-master.sh
+++ build-nigthly-deb-from-master.sh
@@ -21,7 +21,7 @@ UPSTREAM_VERSION=$(git grep AC_INIT origin/standalone:configure.ac | sed -ne 's/
 read DATE SHORTHASH LONGHASH <<<$(git log -1 --date=format:"%Y%m%d%H%M" --pretty=format:"%cd %h %H" origin/standalone)
 DEB_VERSION=${UPSTREAM_VERSION}~${DATE}.${SHORTHASH}
 
-dch -v ${DEB_VERSION}-1 -U "Nightly build from tag ${LONGHASH}"
+dch -D ${DISTRIB} -v ${DEB_VERSION}-1 -U "Nightly build from tag ${LONGHASH}"
 git archive --format=tar --prefix=${PROJECT}-${DEB_VERSION}/ origin/standalone | gzip -c > ${PROJECT}_${DEB_VERSION}.orig.tar.gz
 tar xf ${PROJECT}_${DEB_VERSION}.orig.tar.gz
 cd ${PROJECT}-${DEB_VERSION}
