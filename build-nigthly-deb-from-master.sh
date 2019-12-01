f33a7d8bff247c618fd5b855c1150d302190ba88
Damien Ciabrini <damien.ciabrini@gmail.com>
2019-12-01 16:54:38 +0100

    Configure the branch to build the .deb against

--- build-nigthly-deb-from-master.sh
+++ build-nigthly-deb-from-master.sh
@@ -8,6 +8,7 @@ set -ex
 : ${SIGN_FLAGS:=-us -uc}
 : ${DISTRIB:=UNRELEASED}
 : ${BUILD_OPTS:=-F}
+: ${BRANCH:=origin/master}
 
 export DEBFULLNAME DEBEMAIL
 
@@ -17,12 +18,12 @@ if [ -f "${PUBKEY_FILE}" ]; then
 fi
 
 PROJECT=ngdevkit
-UPSTREAM_VERSION=$(git grep AC_INIT origin/standalone:configure.ac | sed -ne 's/.*\[\(.*\)\].*/\1/p')
-read DATE SHORTHASH LONGHASH <<<$(git log -1 --date=format:"%Y%m%d%H%M" --pretty=format:"%cd %h %H" origin/standalone)
+UPSTREAM_VERSION=$(git grep AC_INIT ${BRANCH}:configure.ac | sed -ne 's/.*\[\(.*\)\].*/\1/p')
+read DATE SHORTHASH LONGHASH <<<$(git log -1 --date=format:"%Y%m%d%H%M" --pretty=format:"%cd %h %H" ${BRANCH})
 DEB_VERSION=${UPSTREAM_VERSION}~${DATE}.${SHORTHASH}
 
 dch -D ${DISTRIB} -v ${DEB_VERSION}-1 -U "Nightly build from tag ${LONGHASH}"
-git archive --format=tar --prefix=${PROJECT}-${DEB_VERSION}/ origin/standalone | gzip -c > ${PROJECT}_${DEB_VERSION}.orig.tar.gz
+git archive --format=tar --prefix=${PROJECT}-${DEB_VERSION}/ ${BRANCH} | gzip -c > ${PROJECT}_${DEB_VERSION}.orig.tar.gz
 tar xf ${PROJECT}_${DEB_VERSION}.orig.tar.gz
 cd ${PROJECT}-${DEB_VERSION}
 cp -a ../debian .
