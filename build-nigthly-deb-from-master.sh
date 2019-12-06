3e25a9663c9669bdd06979aa20c3cacada37094e
Damien Ciabrini <damien.ciabrini@gmail.com>
2019-12-06 23:29:53 +0100

    New numbering scheme, tarball and bionic deps

--- build-nigthly-deb-from-master.sh
+++ build-nigthly-deb-from-master.sh
@@ -20,12 +20,18 @@ fi
 PROJECT=ngdevkit
 UPSTREAM_VERSION=$(git grep AC_INIT ${BRANCH}:configure.ac | sed -ne 's/.*\[\(.*\)\].*/\1/p')
 read DATE SHORTHASH LONGHASH <<<$(git log -1 --date=format:"%Y%m%d%H%M" --pretty=format:"%cd %h %H" ${BRANCH})
-DEB_VERSION=${UPSTREAM_VERSION}~${DATE}.${SHORTHASH}
+UPSTREAM_HASH=${DATE}.${SHORTHASH}
+TARBALL_VERSION=${UPSTREAM_VERSION}~${UPSTREAM_HASH}
+BUILD_INFO=${DISTRIB}.1
+DEB_VERSION=${TARBALL_VERSION}-${BUILD_INFO}
 
-dch -D ${DISTRIB} -v ${DEB_VERSION}-1 -U "Nightly build from tag ${LONGHASH}"
-git archive --format=tar --prefix=${PROJECT}-${DEB_VERSION}/ ${BRANCH} | gzip -c > ${PROJECT}_${DEB_VERSION}.orig.tar.gz
-tar xf ${PROJECT}_${DEB_VERSION}.orig.tar.gz
-cd ${PROJECT}-${DEB_VERSION}
+dch -D ${DISTRIB} -v ${DEB_VERSION} -U "Nightly build from tag ${LONGHASH}"
+git archive --format=tar --prefix=${PROJECT}-${TARBALL_VERSION}/ ${BRANCH} | gzip -nc > ${PROJECT}_${TARBALL_VERSION}.orig.tar.gz
+tar xf ${PROJECT}_${TARBALL_VERSION}.orig.tar.gz
+cd ${PROJECT}-${TARBALL_VERSION}
 cp -a ../debian .
+# unfortunately bionic doesn't package PyGame for python3, so
+# we have to update the Depends fields
+if [ "$DISTRIB" = "bionic" ]; then sed -i 's/, python3-pygame//' debian/control; fi
 yes | mk-build-deps --install --remove
 dpkg-buildpackage -rfakeroot ${BUILD_OPTS} ${SIGN_FLAGS}
