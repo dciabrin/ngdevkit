6d50d4f29c7f8f0ccca05d2bfe53ba2036d32ccf
Damien Ciabrini <damien.ciabrini@gmail.com>
2021-12-25 15:21:54 +0100

    Ability to configure backup RAM segment in ROM
    A new section in ngdevkit linkscript and an associated
    C macro allows variables to be stored in a location that
    gets automatically saved to backup RAM on MVS.
    
    Closes #70

--- include/ngdevkit/backup-ram.h
+++ include/ngdevkit/backup-ram.h
@@ -1,6 +1,6 @@
 /*
- * Header for C development on Neo Geo
- * Copyright (c) 2015-2020 Damien Ciabrini
+ * Backup RAM management on MVS
+ * Copyright (c) 2021 Damien Ciabrini
  * This file is part of ngdevkit
  *
  * ngdevkit is free software: you can redistribute it and/or modify
@@ -17,23 +17,19 @@
  * along with ngdevkit.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-#ifndef __NGDEVKIT_NEOGEO_H__
-#define __NGDEVKIT_NEOGEO_H__
+#ifndef __NGDEVKIT_BACKUP_RAM_H__
+#define __NGDEVKIT_BACKUP_RAM_H__
+
+/** Attribute for variables that need to get saved into backup RAM
+ * The data is automatically saved each time the attract mode is run,
+ * and restored when the ROM is being started.
+ *
+ * Example:
+ *   u8 _backup_ram high_score;
+ *
+ * Note: backup only takes place on MVS hardware
+ */
+#define _backup_ram __attribute__((section(".bss.bram")))
 
 
-/* Shortcut types for C development */
-#include <ngdevkit/types.h>
-
-/* Memory-mapped Neo Geo registers */
-#include <ngdevkit/registers.h>
-
-/* BIOS state variables in memory */
-#include <ngdevkit/bios-ram.h>
-
-/* BIOS system calls */
-#include <ngdevkit/bios-calls.h>
-
-/* MVS: Backup RAM */
-#include <ngdevkit/backup-ram.h>
-
-#endif /* __NGDEVKIT_NEOGEO_H__ */
+#endif /* __NGDEVKIT_BACKUP_RAM_H__ */
