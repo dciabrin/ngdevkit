3a9d95d41ac396294a56c63a6d407180bb033225
Damien Ciabrini <damien.ciabrini@gmail.com>
2023-11-22 21:47:02 +0100

    ADPCM-B support in nullsound and adpcmtool
    New functions to support ADPCM-B playback, with looping
    support and exclusive playback as in ADPCM-A.
    
    Added decoding and encoding of ADPCM-B in adpcmtool.

--- nullsound/adpcm.s
+++ nullsound/adpcm.s
@@ -75,13 +75,23 @@ _adpcm_no_state_update:
 ;;;
 
 ;;; Input for `Play ROM sample` functions below
-        .equ    START_LSB,      0 ; start address * 0x100 in ROM (LSB)
-        .equ    START_MSB,      1 ; start address * 0x100 in ROM (MSB)
-        .equ    STOP_LSB,       2 ; stop address * 0x100 in ROM (LSB)
-        .equ    STOP_MSB,       3 ; stop address * 0x100 in ROM (MSB)
-        .equ    CHANNEL,        4 ; ADPCM-A channel (0..5)
-        .equ    PAN_VOLUME,     5 ; channel L/R ouput + volume
-        .equ    CHANNEL_BIT,    6 ; ADPCM-A channel (2^(channel-1))
+        .equ    A_START_LSB,    0 ; start address * 0x100 in ROM (LSB)
+        .equ    A_START_MSB,    1 ; start address * 0x100 in ROM (MSB)
+        .equ    A_STOP_LSB,     2 ; stop address * 0x100 in ROM (LSB)
+        .equ    A_STOP_MSB,     3 ; stop address * 0x100 in ROM (MSB)
+        .equ    A_CHANNEL,      4 ; channel (0..5)
+        .equ    A_PAN_VOLUME,   5 ; channel L/R ouput + volume
+        .equ    A_CHANNEL_BIT,  6 ; channel (2^(channel-1))
+
+        .equ    B_START_LSB,    0 ; start address * 0x100 in ROM (LSB)
+        .equ    B_START_MSB,    1 ; start address * 0x100 in ROM (MSB)
+        .equ    B_STOP_LSB,     2 ; stop address * 0x100 in ROM (LSB)
+        .equ    B_STOP_MSB,     3 ; stop address * 0x100 in ROM (MSB)
+        .equ    B_DELTA_N_LSB,  4 ; Delta-N (LSB)
+        .equ    B_DELTA_N_MSB,  5 ; Delta-N (MSB)
+        .equ    B_PAN,          6 ; L/R ouput
+        .equ    B_VOLUME,       7 ; volume
+
 
 ;;; Play a ROM sample on a ADPCM-A channel
 ;;; ------
@@ -91,47 +101,47 @@ snd_adpcm_a_play::
         push    bc
         ;; stop playback
         ld      b, #REG_ADPCM_A_START_STOP
-        ld      a, CHANNEL_BIT(ix)
+        ld      a, A_CHANNEL_BIT(ix)
         or      #0x80
         ld      c, a
         call    ym2610_write_port_b
         ;; reset the 'playback finished' flag
         ld      b, #REG_ADPCM_PLAYBACK_MASK
-        ld      c, CHANNEL_BIT(ix)
+        ld      c, A_CHANNEL_BIT(ix)
         call    ym2610_write_port_a
         ld      c, #0
         call    ym2610_write_port_a
         ;; sample start LSB
         ld      a, #REG_ADPCM_A1_ADDR_START_LSB
-        ld      b, CHANNEL(ix)
+        ld      b, A_CHANNEL(ix)
         add     b               ; REG_ADPCM_A<channel>_ADDR_START_LSB
         ld      b, a
-        ld      c, START_LSB(ix)
+        ld      c, A_START_LSB(ix)
         call    ym2610_write_port_b
         ;; sample start MSB
         add     a, #8           ; REG_ADPCM_A<channel>_ADDR_START_MSB
         ld      b, a
-        ld      c, START_MSB(ix)
+        ld      c, A_START_MSB(ix)
         call    ym2610_write_port_b
         ;; sample stop LSB
         add     a, #8           ; REG_ADPCM_A<channel>_ADDR_STOP_LSB
         ld      b, a
-        ld      c, STOP_LSB(ix)
+        ld      c, A_STOP_LSB(ix)
         call    ym2610_write_port_b
         ;; sample stop MSB
         add     a, #8           ; REG_ADPCM_A<channel>_ADDR_STOP_MSB
         ld      b, a
-        ld      c, STOP_MSB(ix)
+        ld      c, A_STOP_MSB(ix)
         call    ym2610_write_port_b
         ;; channel pan and volume
         ld      a, #REG_ADPCM_A1_PAN_VOLUME
-        add     a, CHANNEL(ix)  ; REG_ADPCM_A<channel>_VOL
+        add     a, A_CHANNEL(ix)  ; REG_ADPCM_A<channel>_VOL
         ld      b, a
-        ld      c, PAN_VOLUME(ix)
+        ld      c, A_PAN_VOLUME(ix)
         call    ym2610_write_port_b
         ;; play channel
         ld      b, #REG_ADPCM_A_START_STOP
-        ld      c, CHANNEL_BIT(ix)
+        ld      c, A_CHANNEL_BIT(ix)
         call    ym2610_write_port_b
         ;; mark the channel as busy
         ld      a, (state_adpcm_busy)
@@ -141,6 +151,7 @@ _snd_adpcm_a_end:
         pop     bc
         ret
 
+
 ;;; Play a ROM sample on a ADPCM-A channel only if it's not in use
 ;;; ------
 ;;; ix: sample play config
@@ -148,11 +159,135 @@ _snd_adpcm_a_end:
 snd_adpcm_a_play_exclusive::
         push    bc
         ld      a, (state_adpcm_busy)
-        ld      b, CHANNEL_BIT(ix)
+        ld      b, A_CHANNEL_BIT(ix)
         and     b
         jp      nz, _snd_adpcm_a_busy
         ;; play sample (will mark the channel as busy)
         call    snd_adpcm_a_play
 _snd_adpcm_a_busy:
+        pop     bc
+        ret
+
+
+;;; Configure a ROM sample on the ADPCM-B channel for playback
+;;; ------
+;;; ix: sample play config
+;;; [a modified - other registers saved]
+snd_adpcm_b_play_common::
+        ;; stop playback
+        ld      b, #REG_ADPCM_B_START_STOP
+        ld      c, #0x01
+        call    ym2610_write_port_a
+        ;; reset the 'playback finished' flag
+        ld      b, #REG_ADPCM_PLAYBACK_MASK
+        ld      c, #0x80
+        call    ym2610_write_port_a
+        ld      c, #0x00
+        call    ym2610_write_port_a
+        ;; sample start LSB
+        ld      b, #REG_ADPCM_B_ADDR_START_LSB
+        ld      c, B_START_LSB(ix)
+        call    ym2610_write_port_a
+        ;; sample start MSB
+        ld      b, #REG_ADPCM_B_ADDR_START_MSB
+        ld      c, B_START_MSB(ix)
+        call    ym2610_write_port_a
+        ;; sample stop LSB
+        ld      b, #REG_ADPCM_B_ADDR_STOP_LSB
+        ld      c, B_STOP_LSB(ix)
+        call    ym2610_write_port_a
+        ;; sample stop MSB
+        ld      b, #REG_ADPCM_B_ADDR_STOP_MSB
+        ld      c, B_STOP_MSB(ix)
+        call    ym2610_write_port_a
+        ;; Delta-N LSB
+        ld      b, #REG_ADPCM_B_DELTA_N_LSB
+        ld      c, B_DELTA_N_LSB(ix)
+        call    ym2610_write_port_a
+        ;; Delta-N MSB
+        ld      b, #REG_ADPCM_B_DELTA_N_MSB
+        ld      c, B_DELTA_N_MSB(ix)
+        call    ym2610_write_port_a
+        ;; channel pan
+        ld      b, #REG_ADPCM_B_PAN
+        ld      c, B_PAN(ix)
+        call    ym2610_write_port_a
+        ;; channel volume
+        ld      b, #REG_ADPCM_B_VOLUME
+        ld      c, B_VOLUME(ix)
+        call    ym2610_write_port_a
+        ret
+
+
+;;; Play a ROM sample on the ADPCM-B channel
+;;; ------
+;;; ix: sample play config
+;;; [a modified - other registers saved]
+snd_adpcm_b_play::
+        push    bc
+        call    snd_adpcm_b_play_common
+        ;; play channel
+        ld      b, #REG_ADPCM_B_START_STOP
+        ld      c, #0x80
+        call    ym2610_write_port_a
+        ;; mark the channel as busy
+        ld      a, (state_adpcm_busy)
+        ld      c, #0x80
+        or      c
+        ld      (state_adpcm_busy), a
+        pop     bc
+        ret
+
+
+;;; Play and loop a ROM sample on the ADPCM-B channel
+;;; ------
+;;; ix: sample play config
+;;; [a modified - other registers saved]
+snd_adpcm_b_play_loop::
+        push    bc
+        call    snd_adpcm_b_play_common
+        ;; play channel
+        ld      b, #REG_ADPCM_B_START_STOP
+        ld      c, #0x90
+        call    ym2610_write_port_a
+        ;; mark the channel as busy
+        ld      a, (state_adpcm_busy)
+        ld      c, #0x80
+        or      c
+        ld      (state_adpcm_busy), a
+        pop     bc
+        ret
+
+
+;;; Play a ROM sample on the ADPCM-B channel only if it's not in use
+;;; ------
+;;; ix: sample play config
+;;; [a modified - other registers saved]
+snd_adpcm_b_play_exclusive::
+        push    bc
+        ld      a, (state_adpcm_busy)
+        ld      b, #0x80
+        and     b
+        jp      nz, _snd_adpcm_b_busy
+        ;; play sample (will mark the channel as busy)
+        call    snd_adpcm_b_play
+_snd_adpcm_b_busy:
+        pop     bc
+        ret
+
+
+;;; Play and loop a ROM sample on the ADPCM-B channel only if it's not in use
+;;; ------
+;;; ix: sample play config
+;;; [a modified - other registers saved]
+snd_adpcm_b_play_loop_exclusive::
+        push    bc
+        ld      a, (state_adpcm_busy)
+        ld      b, #0x80
+        and     b
+        jp      nz, _snd_adpcm_b_loop_busy
+        ;; play sample (will mark the channel as busy)
+        call    snd_adpcm_b_play_loop
+_snd_adpcm_b_loop_busy:
         pop     bc
         ret
