dciabrin/ngdevkit

commit 10a6c41e5664b06d393fdf1d13790686f4356f06

NSS: add new opcodes for a more compact stream representation
Three new passes and their associated opcode are implemented:
  - New WAIT_LAST opcode to wait the same amount as previous WAIT
    (saves 1 byte per wait)

  - New opcode to play a NOTE_AND_WAIT_LAST (saves 1 byte per
    play/wait_last)

  - Replace sequence of CALL with a CALL_TABLE and a offset table
    (this saves 2 bytes per additional call to the same pattern)

tools/nsstool.py
@@ -210,14 +210,14 @@ def register_nss_ops():
         ("jmp"     , ["lsb", "msb"]),
         ("nss_end" , ),
         ("tempo"   , ["val"]),
-        ("wait"    , ["rows"]),
+        ("wait_n"  , ["rows"]),
         ("call"    , ["lsb", "msb"]),
         ("nss_ret" , ),
         # 0x08
         ("nop"     , ),
         ("speed"   , ["ticks"]),
         ("groove",   ["ticks"]),
-        None,
+        ("wait_last", ),
         ("b_instr" , ["inst"]),
         ("b_note"  , ["note"]),
         ("b_stop"  , ),
@@ -267,7 +267,7 @@ def register_nss_ops():
         ("fm_pan"  , ["pan_mask"]),
         ("fm_vol_slide_d", ["speed"]),
         ("s_vol_slide_d", ["speed"]),
-        # 0x40
+        # 0x38
         ("fm_pitch_slide_d", ["speed"]),
         ("s_delay" , ["delay"]),
         ("fm_delay", ["delay"]),
@@ -276,7 +276,7 @@ def register_nss_ops():
         ("fm_porta", ["speed"]),
         ("fm_pitch_slide_u", ["speed"]),
         ("s_pitch" , ["pitch"]),
-        # 0x50
+        # 0x40
         ("b_pitch_slide_u", ["speed"]),
         ("s_pitch_slide_u", ["speed"]),
         ("b_porta", ["speed"]),
@@ -285,13 +285,19 @@ def register_nss_ops():
         ("fm_cut",   ["delay"]),
         ("s_cut",    ["delay"]),
         ("a_cut",    ["delay"]),
-        # 0x60
+        # 0x48
         ("b_cut",    ["delay"]),
         ("b_delay",  ["delay"]),
         ("a_retrigger", ["delay"]),
         ("a_pan",    ["pan_mask"]),
         ("b_pan",    ["pan_mask"]),
         ("b_vibrato", ["speed_depth"]),
+        ("call_tbl" , ["calls"]),
+        ("fm_note_w" , ["note"]),
+        # 0x50
+        ("s_note_w"  , ["note"]),
+        ("a_start_w" , ),
+        ("fm_stop_w" , ),
         # reserved opcodes
         ("nss_label", ["pat"])
     )
@@ -302,6 +308,25 @@ def register_nss_ops():
             args=op[1] if len(op)>1 else []
             fields=[(x, int) for x in args]+[cid]
             globals()[cname]=make_dataclass(cname, fields)
+
+
+#
+# Additional internal opcodes
+# generated by optimization passes
+#
+
+@dataclass
+class call_entry:
+    """references a pattern offset in the offset table of a NSS stream"""
+    entry: int
+    _opcode: int = field(default=0, repr=False)
+
+@dataclass
+class pat_offset:
+    """offset of a pattern in bytes from the start of a NSS stream"""
+    lsb: int
+    msb: int
+    _opcode: int = field(default=0, repr=False)
 
 
 def convert_pan(fx, fxval):
@@ -718,7 +743,7 @@ def raw_nss(m, p, bs, channels, compact):
 
             # all channels are processed for this pos.
             # add all generated opcodes plus a time sync
-            pattern_opcodes.extend(opcodes + [wait(1)])
+            pattern_opcodes.extend(opcodes + [wait_n(1)])
 
             # stop processing further rows if a JMP fx was used
             if jmp_to_order != -1:
@@ -737,9 +762,10 @@ def raw_nss(m, p, bs, channels, compact):
             # To account for that, a pattern is identified by its index _and_
             # its length.
             pattern_index = pattern_indices[channels[0]]
-            pattern_waits = [x for x in pattern_opcodes if isinstance(x,wait)]
+            pattern_waits = [x for x in pattern_opcodes if isinstance(x, wait_n)]
             pattern_length = sum([x.rows for x in pattern_waits])
-            pattern_id = "%02x_%x"%(pattern_index,pattern_length)
+            channel = channels[0]
+            pattern_id = "%s_%02x_%02x"%(channel_name(channel),pattern_index,pattern_length)
             if not pattern_id in seen_patterns:
                 # compact representation: labeled pattern that can be jump to
                 pattern_label = nss_label(pattern_id)
@@ -770,24 +796,86 @@ def raw_nss(m, p, bs, channels, compact):
 # NSS optimization passes
 #
 
-def compact_wait(m, nss):
+def merge_adjacent_waits(nss):
     compact = []
     cur_wait = 0
     for op in nss:
-        if type(op) == wait:
+        if type(op) == wait_n:
             cur_wait += op.rows
             # the wait opcode cannot encode more than 255 ticks
             if cur_wait>255:
-                new_wait = wait(255)
+                new_wait = wait_n(255)
                 compact.append(new_wait)
                 cur_wait -= 255
         else:
             if cur_wait>0:
-                new_wait = wait(cur_wait)
+                new_wait = wait_n(cur_wait)
                 compact.append(new_wait)
                 cur_wait=0
             compact.append(op)
     return compact
+
+
+def compact_wait_n_last(nss):
+    last_rows = -1
+
+    def compact_wait_n_last_pass(op, out):
+        nonlocal last_rows
+        if type(op) == nss_label:
+            last_rows = -1
+            out.append(op)
+        elif type(op) == wait_n:
+            if op.rows == last_rows:
+                out.append(wait_last())
+            else:
+                last_rows = op.rows
+                out.append(op)
+        else:
+            out.append(op)
+
+    out = run_control_flow_pass(compact_wait_n_last_pass, nss)
+    return out
+
+
+def fuse_note_wait_last(nss):
+    fuse_map = {fm_note: fm_note_w,
+                s_note: s_note_w,
+                a_start: a_start_w,
+                fm_stop: fm_stop_w}
+    note_op = False
+    start_stop_op = False
+
+    def fuse_note_wait_last_pass(op, out):
+        nonlocal note_op
+        nonlocal start_stop_op
+        if type(op) == wait_last:
+            if note_op:
+                fused_op = fuse_map[type(note_op)](note_op.note)
+                out.append(fused_op)
+                note_op = False
+            elif start_stop_op:
+                fused_op = fuse_map[type(start_stop_op)]()
+                out.append(fused_op)
+                start_stop_op = False
+            else:
+                out.append(op)
+        elif type(op) in [fm_note, s_note]:
+            assert start_stop_op == False
+            note_op = op
+        elif type(op) in [a_start]:
+            assert note_op == False
+            start_stop_op = op
+        else:
+            if note_op:
+                out.append(note_op)
+                note_op = False
+            if start_stop_op:
+                out.append(start_stop_op)
+                start_stop_op = False
+            out.append(op)
+
+    out = run_control_flow_pass(fuse_note_wait_last_pass, nss)
+    return out
 
 
 def compact_instr(nss):
@@ -848,6 +936,47 @@ def compact_instr(nss):
 
     out = run_control_flow_pass(compact_instr_pass, nss)
     return out
+
+
+def compact_calls(nss):
+    compact = []
+
+    offset = {}
+    entries = []
+
+    tmpid = 0
+    def add_call(op):
+        nonlocal tmpid
+        if op.pat not in offset:
+            tmpid -= 1
+            idx = tmpid
+            offset[op.pat] = idx
+        else:
+            idx = offset[op.pat]
+        entries.append(op)
+
+    for op in nss:
+        if type(op) == call:
+            add_call(op)
+        else:
+            if entries:
+                compact.append(call_tbl(len(entries)))
+                for e in entries:
+                    entry = call_entry(offset[e.pat])
+                    entry.pat = e.pat
+                    entry.desc = "for %s"%e.pat
+                    compact.append(entry)
+                entries = []
+            compact.append(op)
+    offsets = []
+    for v, k in sorted([(v,k) for k,v in offset.items()]):
+        o = pat_offset(-1,-1)
+        o.pat = k
+        o.desc = "for %s"%k
+        offsets.append(o)
+    compact = offsets + compact #[nss_label('_start')] + compact
+    return compact
+
 
 
 def tune_adpcm_b_notes(nss, ins):
@@ -932,7 +1061,7 @@ def compact_ctx(nss):
         nonlocal fm_ctx
         nonlocal s_ctx
         nonlocal a_ctx
-        if type(op) == wait:
+        if type(op) in [wait_n, wait_last]:
             fm_ctx=0
             s_ctx=0
             a_ctx=0
@@ -982,7 +1111,7 @@ def run_control_flow_pass(pass_function, nss):
 
     while stream:
         op = stream.pop(0)
-        if type(op) == call:
+        if type(op) in [call, call_entry]:
             out.append(op)
             if op.pat not in seen_blocks:
                 seen_blocks[op.pat] = True
@@ -1026,7 +1155,7 @@ def simulate_ssg_autoenv(nss, ins):
 
     def autoenv_pass(op, out):
         nonlocal s_ctx
-        if type(op) == wait:
+        if type(op) in [wait_n, wait_last]:
             s_ctx=0
             out.append(op)
         elif type(op) == s_macro:
@@ -1069,9 +1198,13 @@ def remove_unreferenced_labels(nss):
     labels = [x for x in nss if isinstance(x, nss_label)]
     callers = [x for x in nss if type(x) in [jmp, call]]
     refs = set([x.pat for x in callers])
+
     out = []
     for op in nss:
-        if isinstance(op, nss_label) and op.pat not in refs:
+        if isinstance(op, nss_label) and op.pat == '_start':
+            # special case for start of stream
+            out.append(op)
+        elif isinstance(op, nss_label) and op.pat not in refs:
             continue
         else:
             out.append(op)
@@ -1082,17 +1215,25 @@ def resolve_jmp_and_call_opcodes(nss):
     labels = {}
     # pass: position of each label in the stream (offset in bytes from start)
     pos = 0
+    start_pos = 0
     for op in nss:
         if isinstance(op, nss_label):
             labels[op.pat] = pos
+            if op.pat == '_start':
+                start_pos = pos
         else:
-            # 1 byte for opcode, + 1 byte per args, - 1 byte for _opcode arg)
-            pos += 1+len(astuple(op))-1
+            # 1 byte per args, do not count the 1 metadata arg
+            pos += len(astuple(op))-1
+            # 1 byte for the opcode ID if required
+            if op._opcode > 0:
+                pos+=1
 
     # pass: resolve jmp and call opcodes
     for op in nss:
-        if type(op) in [jmp, call]:
-            label_offset = labels[op.pat]
+        if type(op) in [jmp, call, pat_offset]:
+            # the real offset is w.r.t the start of the stream,
+            # not counting the call entries
+            label_offset = labels[op.pat] - start_pos
             op.lsb = label_offset & 0xff
             op.msb = (label_offset>>8) & 0xff
 
@@ -1101,11 +1242,9 @@ def resolve_jmp_and_call_opcodes(nss):
 
 def remove_empty_streams(channels, streams):
     def control_flow(op):
-        return type(op) in [jmp, call, nss_ret, nss_label, nss_end, wait]
+        return type(op) in [jmp, call, nss_ret, nss_label, nss_end, wait_n, wait_last]
     def stream_effective_length(s):
         return len([op for op in s if not control_flow(op)])
-    # print(channels)
-    # print(streams[4])
     streams_lengths = [(c, s, stream_effective_length(s)) for c, s in zip(channels, streams)]
     non_empty = [(c, s) for c, s, l in streams_lengths if l > 0]
     return [c for c, s in non_empty], [s for c, s in non_empty]
@@ -1116,8 +1255,13 @@ def stream_size(stream):
         if isinstance(op, nss_label):
             return 0
         else:
-            # size: all fields in the datatype (opcode + args)
-            return len(astuple(op))
+            # size: data size (without metadata fields)
+            size = len(astuple(op)) - 1
+            # if needed, count an additional byte for the opcode ID
+            # if op._count_op or op._opcode > 0:
+            if op._opcode > 0:
+                size+=1
+            return size
 
     sizes = [op_size(op) for op in stream]
     return sum(sizes)
@@ -1136,10 +1280,14 @@ def asm_header(nss, m, name, size, fd):
     print("", file=fd)
 
 
-def stream_name(prefix, channel):
+def channel_name(channel):
     stream_type = ["f1", "f2", "f3", "f4", "s1", "s2", "s3",
                    "a1", "a2", "a3", "a4", "a5", "a6", "b"]
-    return prefix+"_%s"%stream_type[channel]
+    return stream_type[channel]
+
+
+def stream_name(prefix, channel):
+    return prefix+"_%s"%channel_name(channel)
 
 
 def nss_compact_header(mod, channels, streams, name, fd):
@@ -1165,21 +1313,36 @@ def nss_inline_header(channels, name, fd):
 
 
 def nss_to_asm(nss, m, name, fd):
+    def asm_slice(nss):
+        for op in nss:
+            if isinstance(op, nss_label):
+                if op.pat == "_start":
+                    print("        ;; start of NSS stream", file=fd)
+                elif "jmp" not in op.pat:
+                    print("        ;; pattern %s"%(op.pat,), file=fd)
+                continue
+            op_data = []
+            # if op._count_op or op._opcode > 0:
+            if op._opcode > 0:
+                op_data.append(op._opcode)
+            op_data.extend(astuple(op)[:-1])
+            hexdata = ", ".join(["0x%02x"%(x&0xff,) for x in op_data])
+            comment = " ; %s"%type(op).__name__.upper()
+            if "desc" in dir(op):
+                comment+=" "+op.desc
+            # if isinstance(op, call):
+            #     comment+=" "+op.pat
+            print("        .db     "+hexdata.ljust(24)+comment, file=fd)
+
+    start = next(i for i,v in enumerate(nss) if isinstance(v, nss_label) and v.pat == '_start')
+    call_offsets = nss[:start]
+    stream_ops = nss[start:]
+    if call_offsets:
+        print("\n        ;; call entries for %s"%(name,), file=fd)
+        asm_slice(call_offsets)
     if name:
         print("%s::" % name, file=fd)
-    for op in nss:
-        if isinstance(op, nss_label):
-            if "jmp" not in op.pat:
-                print("        ;; pattern %s"%(op.pat,), file=fd)
-            continue
-        opcode = [op._opcode]
-        # remove the last _opcode field, it's just a metadata
-        args = list(astuple(op)[:-1])
-        hexdata = ", ".join(["0x%02x"%(x&0xff,) for x in opcode+args])
-        comment = " ; %s"%type(op).__name__.upper()
-        if isinstance(op, call):
-            comment+=" "+op.pat
-        print("        .db     "+hexdata.ljust(24)+comment, file=fd)
+    asm_slice(stream_ops)
 
 
 def generate_nss_stream(m, p, bs, ins, channels, stream_idx):
@@ -1192,15 +1355,26 @@ def generate_nss_stream(m, p, bs, ins, channels, stream_idx):
         tb = round(256 - (4000000 / (1152 * m.frequency)))
         nss.insert(0, tempo(tb))
 
+    nss.insert(0, nss_label("_start"))
+
     dbg("Transformation passes:")
     dbg(" - remove unreference NSS labels")
     nss = remove_unreferenced_labels(nss)
 
     dbg(" - merge adjacent WAIT opcodes")
-    nss = compact_wait(m, nss)
+    nss = merge_adjacent_waits(nss)
 
     dbg(" - remove successive INSTR opcodes if they keep intrument unchanged")
     nss = compact_instr(nss)
+
+    dbg(" - compact WAIT_N -> WAIT_LAST opcodes")
+    nss = compact_wait_n_last(nss)
+
+    dbg(" - fuse sequences of NOTE / WAIT_LAST opcodes")
+    nss = fuse_note_wait_last(nss)
+
+    dbg(" - compact CALL opcodes into CALL_TABLE opcodes")
+    nss = compact_calls(nss)
 
     dbg(" - tune ADPCM-B notes based on instrument's sample speed")
     nss = tune_adpcm_b_notes(nss, ins)

.github/workflows/build-and-publish.yaml
@@ -68,10 +68,10 @@ jobs:
 
   macos:
     name: "macOS build"
-    runs-on: macos-13
+    runs-on: macos-15
     env:
-      PREFIX: "/usr/local"
-      PY3PATH: "/usr/local/opt/python3/bin"
+      PREFIX: "/opt/homebrew"
+      PY3PATH: "/opt/homebrew/bin"
     steps:
       - name: Checkout
         uses: actions/checkout@v4

.github/workflows/build-tests.yaml
@@ -68,10 +68,10 @@ jobs:
 
   macos:
     name: "macOS build"
-    runs-on: macos-13
+    runs-on: macos-15
     env:
-      PREFIX: "/usr/local"
-      PY3PATH: "/usr/local/opt/python3/bin"
+      PREFIX: "/opt/homebrew"
+      PY3PATH: "/opt/homebrew/bin"
     steps:
       - name: Checkout
         uses: actions/checkout@v4

nullsound/nss-adpcm-a.s
@@ -445,7 +445,6 @@ adpcm_a_configure_vol:
 ;;; ADPCM_A_ON
 ;;; Start sound playback on the current ADPCM-A channel
 ;;; ------
-;;; [ hl ]: ADPCM-A channel [0..5]
 adpcm_a_on::
         ;; delay the start via the trigger FX?
         bit     BIT_TRIGGER_ACTION_DELAY, TRIGGER_ACTION(ix)
@@ -466,6 +465,19 @@ _a_on_end:
         ld      (state_adpcm_a_channel), a
 
         ld      a, #1
+        ret
+
+
+;;; ADPCM_A_ON_AND_WAIT
+;;; Start sound playback on the current ADPCM-A channel and
+;;; immediately wait as many rows as the last wait
+;;; ------
+adpcm_a_on_and_wait::
+        ;; process a regular note opcode
+        call    adpcm_a_on
+
+        ;; wait rows
+        call    wait_last_rows
         ret
 
 

nullsound/nss-fm.s
@@ -1083,6 +1083,20 @@ _fm_note_on_end:
         ret
 
 
+;;; FM_NOTE_ON_AND_WAIT
+;;; Emit a specific note (frequency) on an FM channel and
+;;; immediately wait as many rows as the last wait
+;;; ------
+;;; [ hl ]: note (0xAB: A=octave B=semitone)
+fm_note_on_and_wait::
+        ;; process a regular note opcode
+        call    fm_note_on
+
+        ;; wait rows
+        call    wait_last_rows
+        ret
+
+
 ;;; Release the note on an FM channel and update the pipeline state
 ;;; ------
 fm_stop_playback:

nullsound/nss-ssg.s
@@ -883,6 +883,20 @@ _ssg_note_on_end:
         ret
 
 
+;;; SSG_NOTE_ON_AND_WAIT
+;;; Emit a specific note (frequency) on a SSG channel and
+;;; immediately wait as many rows as the last wait
+;;; ------
+;;; [ hl ]: note (0xAB: A=octave B=semitone)
+ssg_note_on_and_wait::
+        ;; process a regular note opcode
+        call    ssg_note_on
+
+        ;; wait rows
+        call    wait_last_rows
+        ret
+
+
 ;;; SSG_ENV_PERIOD
 ;;; Set the period of the SSG envelope generator
 ;;; ------

nullsound/stream.s
@@ -27,6 +27,7 @@
         .equ    CH_STREAM_SAVED, (state_ch_stream_saved_pos-state_ch_stream)
         .equ    CH_STREAM_START, (state_ch_stream_start-state_ch_stream)
         .equ    CH_STREAM_POS, (state_ch_stream_pos-state_ch_stream)
+        .equ    CH_STREAM_ENTRIES, (state_ch_stream_entries-state_ch_stream)
         .equ    CH_STREAM_SIZE, (state_ch_stream_end-state_ch_stream)
         .equ    NB_YM2610_CHANNELS, 14
 
@@ -75,6 +76,13 @@ state_ch_ctx_switch::           .blkb   NB_YM2610_CHANNELS
 ;;; the NSS data gets a dedicated wait state
 state_ch_wait_rows::           .blkb   NB_YM2610_CHANNELS
 
+;;; per-channel wait opcode cache
+;;; ---
+;;; Wait value used by the last wait opcode.
+;;; When multiple streams are used, each YM2610 channel used in
+;;; the NSS data gets a dedicated wait state
+state_ch_wait_op_val::         .blkb   NB_YM2610_CHANNELS
+
 ;;; per-channel playback state
 ;;; ---
 ;;; Keep track of positional information for streams.
@@ -87,14 +95,23 @@ state_ch_stream:
 state_ch_stream_saved_pos::     .blkb   2
 state_ch_stream_start::         .blkb   2
 state_ch_stream_pos::           .blkb   2
+state_ch_stream_entries::       .blkb   1
 state_ch_stream_end:
         .blkb   CH_STREAM_SIZE*(NB_YM2610_CHANNELS-1)
 
 ;;; addresses/indices that points to state of the currently processed stream
 state_current_ch_ctx::          .blkb   2
 state_current_ch_wait_rows::    .blkb   2
+state_current_ch_wait_op_val::  .blkb   2
 state_current_ch_stream::       .blkb   2
 state_stream_idx::              .blkb   1
+
+;;; generic function pointer for current NSS processing function
+state_nss_process::
+        .blkb   1         ; jp
+state_nss_process_func::
+        .blkb   2         ; func offset in ROM
+
 
         ;; FIXME: temporary padding to ensures the next data sticks into
         ;; a single 256 byte boundary to make 16bit arithmetic faster
@@ -109,6 +126,8 @@ init_stream_state_tracker::
         ld      (state_stream_in_use), a
         ld      bc, #0
         ld      (state_stream_instruments), bc
+        ld      a, #0xc3        ; jp
+        ld      (state_nss_process), a
         ;; init nss subsystems that may get called prior to playing music
         call    init_nss_fm_state_tracker
         ret
@@ -151,6 +170,8 @@ process_streams_opcodes::
         ld      (state_stream_idx), a
         ld      bc, #state_ch_wait_rows
         ld      (state_current_ch_wait_rows), bc
+        ld      bc, #state_ch_wait_op_val
+        ld      (state_current_ch_wait_op_val), bc
         ld      bc, #state_ch_ctx_switch
         ld      (state_current_ch_ctx), bc
         ld      bc, #state_ch_stream
@@ -175,7 +196,7 @@ _loop_chs:
         ld      l, CH_STREAM_POS(iy)
         ld      h, CH_STREAM_POS+1(iy)
 _loop_opcode:
-        call    process_nss_opcode
+        call    state_nss_process
         or      a
         jp      nz, _loop_opcode
         ;; no more opcodes can be processed, save stream's new pos
@@ -193,6 +214,9 @@ _post_ch_process:
         ld      hl, (state_current_ch_wait_rows)
         inc     hl
         ld      (state_current_ch_wait_rows), hl
+        ld      hl, (state_current_ch_wait_op_val)
+        inc     hl
+        ld      (state_current_ch_wait_op_val), hl
         ld      hl, (state_current_ch_ctx)
         inc     hl
         ld      (state_current_ch_ctx), hl
@@ -397,6 +421,10 @@ stream_play_multi::
         push    iy
         pop     hl
 
+        ;; setup the generic NSS processing function
+        ld      bc, #process_nss_opcode
+        ld      (state_nss_process_func), bc
+
         ;; init streams state
         ld      iy, #state_ch_stream
         ld      de, #CH_STREAM_SIZE
@@ -460,13 +488,13 @@ nss_opcodes:
         .nss_op nss_jmp
         .nss_op nss_end
         .nss_op timer_tempo
-        .nss_op wait_rows
+        .nss_op wait_n_rows
         .nss_op nss_call
         .nss_op nss_ret
         .nss_op nss_nop
         .nss_op row_speed
         .nss_op row_groove
-        .nss_op nss_nop2
+        .nss_op wait_last_rows
         .nss_op adpcm_b_instrument
         .nss_op adpcm_b_note_on
         .nss_op adpcm_b_note_off
@@ -533,6 +561,10 @@ nss_opcodes:
         .nss_op adpcm_a_pan
         .nss_op adpcm_b_pan
         .nss_op adpcm_b_vibrato
+        .nss_op nss_call_table
+        .nss_op fm_note_on_and_wait
+        .nss_op ssg_note_on_and_wait
+        .nss_op adpcm_a_on_and_wait
 
 
 
@@ -630,16 +662,19 @@ nss_end::
         ret
 
 
-;;; WAIT_ROWS
+;;; WAIT_N_ROWS
 ;;; Suspend stream playback, resume after a number of rows
 ;;; worth of time has passed (Timer B interrupts * speed).
 ;;; ------
 ;;; [hl]: number of interrupts until playback resumes
-wait_rows::
+wait_n_rows::
         push    bc
         ;;  how many interrupts to wait for before moving on
         ld      a, (hl)
         inc     hl
+        ;; recall this wait value for this channel
+        ld      bc, (state_current_ch_wait_op_val)
+        ld      (bc), a
         ;; register the wait for this channel
         ld      bc, (state_current_ch_wait_rows)
         ld      (bc), a
@@ -652,6 +687,22 @@ _post_wait_rows:
         pop     bc
         ld      a, #0
         ret
+
+
+;;; WAIT_LAST_ROWS
+;;; Suspend stream playback, resume after a number of rows
+;;; worth of time has passed (same as the last wait_n_rows)
+;;; ------
+wait_last_rows::
+        push    bc
+        ;; last wait for this channel
+        ld      bc, (state_current_ch_wait_op_val)
+        ld      a, (bc)
+        ;; register the wait for this channel
+        ld      bc, (state_current_ch_wait_rows)
+        ld      (bc), a
+
+        jp      _post_wait_rows
 
 
 ;;; NSS_CALL
@@ -687,6 +738,82 @@ nss_call::
         ret
 
 
+;;; NSS_CALL_TABLE
+;;; Set up a series of calls to different locations in the stream
+;;; ------
+;;; [ hl ]: number of calls
+nss_call_table::
+        ;; bc: number of calls
+        ld      a, (hl)
+        inc     hl
+
+        ;; recall the number of entries in the call table
+        ld      iy, (state_current_ch_stream)
+        ld      CH_STREAM_ENTRIES(iy), a
+
+        push    bc
+        ld      bc, #process_table_entry
+        ld      (state_nss_process_func), bc
+        pop     bc
+
+        ld      a, #1
+        ret
+
+
+;;; Continue playback to a new position in the stream
+;;; Recall the current position so that a NSS_RET opcode
+;;; continues execution from there.
+;;; Note: no CALL entry can be processed again before a NSS_RET
+;;; ------
+;;; [ hl ]: pattern offset in the entry table
+process_table_entry::
+        push    bc
+
+        ;; a: entry
+        ld      a, (hl)
+        inc     hl
+
+        ld      iy, (state_current_ch_stream)
+
+        ;; save current stream pos
+        ld      CH_STREAM_SAVED(iy), l
+        ld      CH_STREAM_SAVED+1(iy), h
+
+        ;; hl: start of stream
+        ld      l, CH_STREAM_START(iy)
+        ld      h, CH_STREAM_START+1(iy)
+
+        ;; hl: offset in entry table
+        ld      b, #0xff
+        ld      c, a
+        add     hl, bc
+        add     hl, bc
+
+        ;; bc: pattern offset
+        ld      c, (hl)
+        inc     hl
+        ld      b, (hl)
+
+        ;; hl: start of stream
+        ld      l, CH_STREAM_START(iy)
+        ld      h, CH_STREAM_START+1(iy)
+
+        ;; hl: new pos (call offset)
+        add     hl, bc
+        ld      CH_STREAM_POS(iy), l
+        ld      CH_STREAM_POS+1(iy), h
+
+        ;; record that one call entry has been processed
+        dec     CH_STREAM_ENTRIES(iy)
+
+        ld      bc, #process_nss_opcode
+        ld      (state_nss_process_func), bc
+
+        pop     bc
+        ld      a, #1
+        ret
+
+
 ;;; NSS_RET
 ;;; Continue playback past the previous NSS_CALL statement
 ;;; ------
@@ -698,6 +825,16 @@ nss_ret::
         ;; hl: restore new stream pos
         ld      CH_STREAM_POS(iy), l
         ld      CH_STREAM_POS+1(iy), h
+
+        push    bc
+        ld      bc, #process_nss_opcode
+        xor     a
+        cp      CH_STREAM_ENTRIES(iy)
+        jr      z, _ret_set_process
+        ld      bc, #process_table_entry
+_ret_set_process:
+        ld      (state_nss_process_func), bc
+        pop     bc
 
         ld      a, #1
         ret

commit 822dde887ec7537a3a1374032052ac93e268ace8

NSS: do not generate NSS for unused tracks
This got broken in 10a6c41e5664b06d393fdf1d13790686f4356f06 with the
introduction of a compact call table opcode.

tools/nsstool.py
@@ -1300,17 +1300,13 @@ def resolve_jmp_and_call_opcodes(nss):
     return nss
 
 
-def remove_empty_streams(channels, streams):
+def stream_size_in_effective_opcodes(stream):
     def control_flow(op):
-        return type(op) in [jmp, call, nss_ret, nss_label, nss_end, wait_n, wait_last]
-    def stream_effective_length(s):
-        return len([op for op in s if not control_flow(op)])
-    streams_lengths = [(c, s, stream_effective_length(s)) for c, s in zip(channels, streams)]
-    non_empty = [(c, s) for c, s, l in streams_lengths if l > 0]
-    return [c for c, s in non_empty], [s for c, s in non_empty]
+        return type(op) in [jmp, call, pat_offset, call_tbl, call_entry, nss_ret, nss_label, nss_end, wait_n, wait_last]
+    return len([op for op in stream if not control_flow(op)])
 
 
-def stream_size(stream):
+def stream_size_in_bytes(stream):
     def op_size(op):
         if isinstance(op, nss_label):
             return 0
@@ -1412,15 +1408,25 @@ def nss_to_asm(nss, m, name, fd):
     asm_slice(stream_ops)
 
 
+def remove_empty_streams(channels, streams):
+    streams_size = [(c, s, stream_size_in_effective_opcodes(s)) for c, s in zip(channels, streams)]
+    non_empty = [(c, s) for c, s, l in streams_size if l > 0]
+    return [c for c, s in non_empty], [s for c, s in non_empty]
+
+
+tempo_injected=False
 def generate_nss_stream(m, p, bs, ins, channels, stream_idx):
     compact = stream_idx >= 0
 
     dbg("Convert Furnace patterns to unoptimized NSS opcodes")
     nss = raw_nss(m, p, bs, channels, compact)
 
-    if stream_idx <= 0:
+    # insert a tempo opcode on the first track that is used in the Furnace module
+    global tempo_injected
+    if not tempo_injected and stream_size_in_effective_opcodes(nss)>0:
         tb = round(256 - (4000000 / (1152 * m.frequency)))
         nss.insert(0, tempo(tb))
+        tempo_injected = True
 
     nss.insert(0, nss_label("_start"))
 
@@ -1537,7 +1543,7 @@ def main():
                 1 + len(m.speeds) +  # speeds
                 (2 * len(streams)))  # stream pointers
         # all streams sizes
-        size += sum([stream_size(s) for s in streams])
+        size += sum([stream_size_in_bytes(s) for s in streams])
         asm_header(streams, m, name, bank, size, outfd)
         nss_compact_header(m, channels, streams, name, outfd)
         for i, ch, stream in zip(range(len(channels)), channels, streams):
@@ -1545,7 +1551,7 @@ def main():
     else:
         stream = generate_nss_stream(m, p, bs, ins, channels, -1)
         # NSS inline marker + channels bitfield, stream size
-        size = 1 + 2 + stream_size(stream)
+        size = 1 + 2 + stream_size_in_bytes(stream)
         asm_header(stream, m, name, bank, size, outfd)
         nss_inline_header(channels, name, outfd)
         nss_to_asm(stream, m, False, outfd)